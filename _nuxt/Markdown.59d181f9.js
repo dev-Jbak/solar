0e3287a00cf9853c77f853e7de8398df94da6e65
dev-Jbak <87811523+dev-Jbak@users.noreply.github.com>
2023-04-09 21:09:33 +0100

    Updates

---
+++ _nuxt/Markdown.59d181f9.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.1a9d6501.js";import{f as o,p as f,x as p,F as u}from"./entry.08668a4a.js";import"./utils.9c5d6c83.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=u(),{between:n,default:a}=f(),r=p(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
